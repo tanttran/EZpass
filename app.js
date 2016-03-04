43a2191ac81e86dc7e4d07e07b90855abc3904a6
tantran <octttran@gmail.com>
2016-03-03 21:53:25 -0800

    basic web with initial homepage register and login

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
+
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/login.html'));
+});
+
+app.get('/register', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/register.html'));
+});
+
+
+app.listen(1337);
+console.log('Server is running 13337');
